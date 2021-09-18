3dbebffb7bde84634a9cb022a57c21f017e9bbfe
Joseph Miller <millerjoseph449@gmail.com>
2021-09-18 11:41:51 -0400

    problems with packaging

--- src/main/java/program/MainFrame.java
+++ src/main/java/program/MainFrame.java
@@ -1,8 +1,17 @@
-
-package src.main.java.program;
+package program;
 
 import src.main.java.utils.ScreenDimension;
 import javax.swing.*;
+import java.awt.*;
+
+/**
+ * MainFrame
+ *
+ * Creates the basic frame for the program that contains all components
+ *
+ * @author Joseph Miller,
+ * @version September 18, 2021
+ */
 
 public class MainFrame extends JFrame{
 
@@ -10,9 +19,12 @@ public class MainFrame extends JFrame{
         System.out.println();
     }
 
-    private int mainFrameWidth = ScreenDimension.getScreenWidth();
+    private int mainFrameWidth = ScreenDimension.getScreenWidth() / 2 / 3;
+    private int mainFrameHeight = ScreenDimension.getScreenHeight() / 2 / 3;
+    private Dimension mainFrameSize = new Dimension(mainFrameWidth, mainFrameHeight);
 
     public MainFrame() {
+
 
     }
 }
