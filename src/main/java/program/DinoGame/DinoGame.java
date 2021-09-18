b32c8301d61086a1c4d542f782b57018ade1d5a8
Jacob A <jacobraldridge@gmail.com>
2021-09-18 15:32:50 -0400

    Added collisions

--- src/main/java/program/DinoGame/DinoGame.java
+++ src/main/java/program/DinoGame/DinoGame.java
@@ -1,7 +1,9 @@
-package src.main.java.program.DinoGame;
+package main.java.program.DinoGame;
 
 import java.awt.*;
 import java.awt.event.*;
+
+import javax.swing.JFrame;
 
 public class DinoGame extends Canvas implements KeyListener {
 
@@ -11,45 +13,53 @@ public class DinoGame extends Canvas implements KeyListener {
     private Dinosaur dino = new Dinosaur();
     private Cactus cactus = new Cactus();
     
+    private double fps = 30;
     private long timer = 0;
 
     public DinoGame() {
-        Frame frame = new Frame("Dino Game");
+        JFrame frame = new JFrame("Dino Game");
         setBackground(Color.white);
 
         addKeyListener(this);
         frame.add(this);
         frame.setSize(width, height);
         frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         repaint();
     }
 
-    public void paint(Graphics g) { }
-
     public void update(Graphics g) {
         timer = System.currentTimeMillis();
 
-        if(timer % 33 == 0) {
-        cactus.moveCactus();
-        dino.moveDino();
+        if(timer % (int)(1000 / fps) == 0) {
+            cactus.moveCactus();
+            dino.moveDino();
 
-        // Draw background because it's not actually white
-        g.setColor(Color.white);
-        g.fillRect(-1, -1, width + 2, height + 2);
+            // Draw background because it's not actually white
+            g.setColor(Color.white);
+            g.fillRect(-1, -1, width + 2, height + 2);
 
-        // Draw stuff
-        dino.paintDino(g);
-        cactus.paintCactus(g);
+            // Draw stuff
+            dino.paintDino(g);
+            cactus.paintCactus(g);
 
-        // Draw ground
-        g.setColor(Color.black);
-        g.fillRect(0, DinoConstants.GROUND_LEVEL, width, 3);
+            // Draw ground
+            g.setColor(Color.black);
+            g.fillRect(0, DinoConstants.GROUND_LEVEL, width, 3);
 
-        // Test boundaries
-        g.setColor(Color.red);
-        g.drawLine(0, cactus.getTopBoundary(), width, cactus.getTopBoundary());
-        g.drawLine(cactus.getLeftBoundary(), 0, cactus.getLeftBoundary(), height);        
+            // Test boundaries
+            g.setColor(Color.red);
+            g.drawLine(0, cactus.getTopBoundary(), width, cactus.getTopBoundary());
+            g.drawLine(cactus.getLeftBoundary(), 0, cactus.getLeftBoundary(), height);   
+            g.drawLine(0, dino.getBottomBoundary(), width, dino.getBottomBoundary());
+            g.drawLine(dino.getRightBoundary(), 0, dino.getRightBoundary(), height);  
+            
+            if(cactus.getTopBoundary() < dino.getBottomBoundary() && 
+            cactus.getLeftBoundary() < dino.getRightBoundary()) {
+                fps = 1 / Integer.MAX_VALUE;
+                //dino.paintDeadDino(g);
+            }   
         }
         repaint();
     }
