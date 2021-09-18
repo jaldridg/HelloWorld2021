2a359635c0410f09ce54d776366140546ba45a54
Jacob A <jacobraldridge@gmail.com>
2021-09-18 12:52:10 -0400

    Positioned the dinosaur and made the ground

--- src/main/java/program/DinoGame/DinoGame.java
+++ src/main/java/program/DinoGame/DinoGame.java
@@ -7,25 +7,45 @@ import javax.swing.JFrame;
 public class DinoGame extends Canvas implements KeyListener {
 
     final int PIXEL_SIZE = 5;
+    final int WIDTH = 1000;
+    final int HEIGHT = 500;
+
+    final int GROUND_LEVEL = 300;
+
+    final int[][] dinosaurPixels = PixelArrays.DINOSAUR_PIXELS;
     
     public DinoGame() {
         JFrame frame = new JFrame("Dino Game");
 
         frame.add(this);
-        frame.setSize(1000, 500);
+        frame.setSize(WIDTH, HEIGHT);
         frame.setVisible(true);
+
+        frame.setBackground(Color.white);
 
         repaint();
     }
 
     public void paint(Graphics g) {
-        for(int i = 0; i < PixelArrays.DinosaurPixels.length; i++) {
-            for(int j = 0; j < PixelArrays.DinosaurPixels[0].length; j++) {
-                int color = PixelArrays.DinosaurPixels[i][j] * 255;
+        // Draw background because it's not actually white
+        g.setColor(Color.white);
+        g.fillRect(-1, -1, WIDTH + 2, HEIGHT + 2);
+
+        // Draw dinosaur
+        int dinosaurHeight = (dinosaurPixels.length - 3) * PIXEL_SIZE;
+        for(int i = 0; i < dinosaurPixels.length; i++) {
+            for(int j = 0; j < dinosaurPixels[0].length; j++) {
+                int color = Math.abs(dinosaurPixels[j][i] - 1) * 255;
                 g.setColor(new Color(color, color, color));
-                g.fillRect(i * PIXEL_SIZE, j * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
+                g.fillRect(i * PIXEL_SIZE + GROUND_LEVEL - 200, 
+                           j * PIXEL_SIZE + GROUND_LEVEL - dinosaurHeight, 
+                           PIXEL_SIZE, 
+                           PIXEL_SIZE);
             }
         }
+        // Draw ground
+        g.setColor(Color.black);
+        g.fillRect(0, GROUND_LEVEL, WIDTH, 3);
     }
 
     public void update(Graphics g) {
@@ -34,7 +54,9 @@ public class DinoGame extends Canvas implements KeyListener {
 
     public void keyTyped(KeyEvent e) { }
     public void keyReleased(KeyEvent e) { }
-    public void keyPressed(KeyEvent e) { }
+    public void keyPressed(KeyEvent e) { 
+
+    }
 
     public static void main(String[] args) {
         DinoGame dg = new DinoGame();
