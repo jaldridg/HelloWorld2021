b94dbd1b66626f2e9c398569c8fbb00f62cd688e
Joseph Miller <millerjoseph449@gmail.com>
2021-09-18 11:00:13 -0400

    Added a class ScreenDimension that contains methods with access to current screen resolution dimensions.

---
+++ src/main/java/utils/ScreenDimensions.java
@@ -0,0 +1,29 @@
+import java.awt.*;
+
+/**
+ * ScreenDimension
+ *
+ * Subclass of dimension specifically for holding the dimensions of the screen resolution
+ *
+ * @author Joseph Miller
+ * @version September 10, 2021
+ *
+ */
+
+public class ScreenDimension extends Dimension {
+
+    private static final Dimension screenRes = Toolkit.getDefaultToolkit().getScreenSize();
+
+    public static int getScreenHeight() {
+        return screenRes.height;
+    }
+
+    public static int getScreenWidth() {
+        return screenRes.width;
+    }
+
+    public static Dimension getScreenDim() {
+        return screenRes;
+    }
+
+}
